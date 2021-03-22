95fefe65a335ebbde7f4fe3ec8dd2ebe55b0a915
dharmeshkumar1409 <73641326+dharmeshkumar1409@users.noreply.github.com>
2021-03-22 16:23:03 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,20 @@
+class Bird extends BaseClass {
+    constructor(x, y) {
+        super(x, y, 60, 60);
+        this.image = loadImage("sprites/bird.png");
+        this.image1 = loadImage("sprites/smoke.png");
+        this.path = [];
+    }
+
+    display() {
+        super.display();
+        if (this.body.velocity.x > 10 && this.body.position.x > 200) {
+            var pos = [this.body.position.x, this.body.position.y];
+            this.path.push(pos);
+        }
+        for (var i = 0; i < this.path.length; i++) {
+            image(this.image1, this.path[i][0], this.path[i][1]);
+        }
+
+    }
+}
